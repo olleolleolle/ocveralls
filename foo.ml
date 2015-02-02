a36007f8d6cc8bd47a2d72455f8de39623f90edd
Julien Sagot <ju.sagot@gmail.com>
2015-02-02 23:51:11 +0100

    Make the build pass removing dummy failing test.

--- foo.ml
+++ foo.ml
@@ -5,7 +5,5 @@ let bar = fun () -> "bar"
 let baz = fun () -> "baz"
 
 let _ =
-  "test" >:::
-    [ "foo" >:: (fun () -> assert_equal "foo" (foo ())) ;
-      "bar" >:: (fun () -> assert_equal "foo" (bar ())) ]
-  |> OUnit.run_test_tt_main
+  run_test_tt_main
+    ("foo" >::: [ "foo" >:: (fun () -> assert_equal "foo" (foo ())) ])
