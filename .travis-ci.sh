353e88994896acd0be51b1f84289af30c22efb57
Julien Sagot <ju.sagot@gmail.com>
2015-02-02 22:28:54 +0100

    Added -n option to pin command. Removed 'ln -s' loop.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,4 @@
-OPAM_DEPENDS="ocamlfind ounit menhir bisect"
+OPAM_DEPENDS="ocamlfind ounit menhir bisect ocveralls"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
     3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
@@ -18,22 +18,14 @@ sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 
 export OPAMYES=1
 opam init
-
-git clone https://github.com/sagotch/ezjsonm.git
-opam pin add -k git ezjsonm ezjsonm
-
-git clone https://github.com/sagotch/ocveralls.ml.git
-opam pin add -k git ocveralls ocveralls.ml
-
-opam install ${OPAM_DEPENDS}
 eval `opam config env`
 
-# patch broken path lookup
-pushd `opam config var bin`
-for bin in /usr/bin/*caml*
-do
-    ln -s $bin
-done
-popd
+git clone https://github.com/sagotch/ezjsonm.git
+opam pin add -k git -n ezjsonm ezjsonm
+
+git clone https://github.com/sagotch/ocveralls.ml.git
+opam pin add -k git -n ocveralls ocveralls.ml
+
+opam install ${OPAM_DEPENDS}
 
 make coverage coveralls
