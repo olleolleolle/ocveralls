787bd8e4abf72e70e83998902f094bd3b7591f24
Julien Sagot <ju.sagot@gmail.com>
2015-02-13 18:19:36 +0100

    Changed OCAML_VERSION and OPAM_VERSION.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,14 +1,8 @@
 OPAM_DEPENDS="ocamlfind ounit bisect ocveralls"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
-    3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-    3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-    4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-    4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-    4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
-    4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
     4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
-    4.02.0,1.2.0) ppa=avsm/ocaml42+opam12 ;;
+    4.02.1,1.2.0) ppa=avsm/ocaml42+opam12 ;;
     *) error "Unknown ocaml or opam version: $OCAML_VERSION,$OPAM_VERSION";;
 esac
 
